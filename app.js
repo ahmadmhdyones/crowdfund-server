cd3c9b665265b3d89198c09b5ba742e317553abb
Ibrahim Hafez <ibrahim.hafez99@hotmail.com>
2023-01-03 13:58:50 +0300

    Install cors and configure it

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ import express from 'express';
 import morgan from 'morgan';
 import colors from 'colors';
 import router from './routes/index.js';
+import cors from 'cors';
 
 dotenv.config({ path: './.env' });
 
 const app = express();
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(router);
