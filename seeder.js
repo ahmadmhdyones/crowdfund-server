5095bae019bcab5606be397a464b11c402625405
Ahmad Yones <ahmadmhdyones@gmail.com>
2023-01-21 02:47:18 +0300

    Add new approved campaign
    - Update seeder file.
    - Add new campaign by admin user id.
    - Set new campaign as approved.

--- seeder.js
+++ seeder.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import users from './data/users.js';
 import User from './models/userModel.js';
+import Campaign from './models/campaignModel.js';
 import connectDB from './config/db.js';
 import Campagin from './models/campaignModel.js';
 import Contribution from './models/contributionModel.js';
@@ -15,6 +16,22 @@ const importData = async () => {
     const createdUsers = await User.insertMany(users);
 
     const adminUser = createdUsers[0]._id;
+
+    // Add new campaign as approved
+    const campaign = new Campaign({
+      user: createdUsers[0]._id,
+      name: createdUsers[0].name,
+      title: 'Test Campagin',
+      description:
+        'Culpa aliquip est sint aliqua magna laboris eu quis duis tempor nisi occaecat.',
+      image:
+        'https://cdn4.buysellads.net/uu/1/81016/1609783186-authentic-260x200-variation-2.jpg',
+      goal: 0.05,
+      endAt: '2025-05-25T08:33:42.905+00:00',
+      minPledge: 0,
+      state: 'approved'
+    });
+    await campaign.save();
 
     console.log('✅ Data Imported!'.green);
     process.exit();
