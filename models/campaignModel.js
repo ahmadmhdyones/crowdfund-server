9105f2612f304174c1cd94f3c44d8b559b9a4c0e
Ahmad Yones <ahmadmhdyones@gmail.com>
2023-01-21 05:32:34 +0300

    Remove requests from campaign model

--- models/campaignModel.js
+++ models/campaignModel.js
@@ -1,42 +1,5 @@
 import mongoose from 'mongoose';
 
-// TODO: move to single file
-const requestSchema = new mongoose.Schema({
-  index: {
-    type: Number,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (x) => x > 0,
-      message: 'Index must be positive'
-    }
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  amount: {
-    type: Number,
-    required: true,
-    min: [1, 'Should be greater than zero, got {VALUE}']
-  },
-  recipient: {
-    type: String,
-    required: true
-  },
-  isCompleted: {
-    type: Boolean,
-    required: false,
-    default: false
-  },
-  countApprovals: {
-    type: Number,
-    required: false,
-    default: 0,
-    min: [0, 'Should not be negative, got {VALUE}']
-  }
-});
-
 const campaginSchema = new mongoose.Schema(
   {
     user: {
@@ -102,12 +65,6 @@ const campaginSchema = new mongoose.Schema(
       required: false,
       default: 0,
       min: [0, 'Should not be negative, got {VALUE}']
-    },
-    // TODO: move to single file
-    requests: {
-      type: [requestSchema],
-      required: false,
-      default: []
     },
     state: {
       type: String,
